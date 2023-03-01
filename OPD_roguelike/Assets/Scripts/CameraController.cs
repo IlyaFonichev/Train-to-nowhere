acdb7ebb64772f54f3b8515cbeb33abe1e2327eb
Razzle131 <Razzle465@gmail.com>
2023-03-02 02:22:53 +0300

    small changes
    исправил нотацию, перекинул часть скрипта с playercontroller на cameracontroller

---
+++ OPD_roguelike/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject scope;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float smoothSpeed = 0.125f;
+
+    private RectTransform scope_rt;
+    private float deltaPosX, deltaPosY, deltaPosZ;
+
+    private void Start()
+    {
+        scope_rt = scope.GetComponent<RectTransform>();
+
+        // „тобы камеру потом можно было двигать
+        deltaPosX = transform.position.x - player.transform.position.x;
+        deltaPosY = transform.position.y - player.transform.position.y;
+        deltaPosZ = transform.position.z - player.transform.position.z;
+
+    }
+
+    private void FixedUpdate()
+    {
+        showScope();
+        followPlayer();
+    }
+
+    private void showScope()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        scope_rt.anchoredPosition = mousePos;
+    }
+
+    private void followPlayer()
+    {
+        Vector3 target = player.transform.position + new Vector3(deltaPosX, deltaPosY, deltaPosZ);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, target, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ OPD_roguelike/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float dashSpeed = 1f;
+    [SerializeField] private AnimationCurve dashSpeedCurve;
+    [SerializeField] private float dashTime = 0.5f;
+    [SerializeField] private GameObject shot;
+    
+    private Rigidbody rb;
+    private bool isDashing;
+    private Vector3 canvasCenter;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();       
+
+        // Для вычисления вектора направления стрельбы
+        canvasCenter = new Vector3(Screen.width / 2, Screen.height / 2, playerCamera.nearClipPlane);
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(moveHorizontal, 0, moveVertical);
+
+        move(direction);
+        StartCoroutine(shoot());
+        StartCoroutine(Dash(direction));      
+    }
+
+    private void move(Vector3 direction)
+    {
+        if (isDashing) return;
+        rb.velocity = direction.normalized * speed;
+    }
+
+    private IEnumerator shoot()
+    {
+        if (Input.GetAxisRaw("Fire") == 0) yield break;
+        if (isDashing) yield break;
+
+        Vector3 mousePos2D = Input.mousePosition;
+
+        Vector3 mousePosNearClipPlane = new Vector3(mousePos2D.x, mousePos2D.y, playerCamera.nearClipPlane);
+        
+        Vector3 worldPointPos = playerCamera.ScreenToWorldPoint(mousePosNearClipPlane);
+        Vector3 currCanvasCenter = playerCamera.ScreenToWorldPoint(canvasCenter);
+
+        Vector3 mouseVector = worldPointPos - currCanvasCenter;
+        mouseVector = new Vector3(mouseVector.x, 0, mouseVector.z);
+
+        mouseVector.Normalize();
+
+        GameObject bullet = Instantiate(shot, transform.position + (mouseVector) * 2, new Quaternion(0,0,0,0));
+
+        float elapsedtime = 0f;
+        while (elapsedtime < 1)
+        {
+            bullet.transform.Translate(mouseVector * speed * Time.fixedDeltaTime * dashSpeed);
+
+            elapsedtime += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        Destroy(bullet);
+
+        yield break;
+    }
+
+    private IEnumerator Dash(Vector3 direction)
+    {
+        if (Input.GetAxisRaw("Roll") == 0) yield break;
+        if (direction == Vector3.zero) yield break;
+        if (isDashing) yield break;
+
+        isDashing = true;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < dashTime)
+        {
+            rb.velocity = direction.normalized * speed * Time.fixedDeltaTime * dashSpeed;
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        isDashing = false;
+        yield break;
+    }
+}
