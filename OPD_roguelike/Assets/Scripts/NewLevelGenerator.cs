9726b0c07cb7a362bec197feab548f2d60bb07fe
PashtetDev <pashtetdev@gmail.com>
2023-03-02 21:22:23 +0300

    Add

--- OPD_roguelike/Assets/Scripts/NewLevelGenerator.cs
+++ OPD_roguelike/Assets/Scripts/NewLevelGenerator.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class NewLevelGenerator : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject gang;
+    private void Start()
+    {
+        //         объект, Место спавна,  Поворот
+        Instantiate(gang, Vector3.zero, Quaternion.Euler(90, 0, 0));
+    }
 }
