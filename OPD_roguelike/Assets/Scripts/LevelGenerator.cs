249ad87568d70bc150558156d500eac195487440
PashtetDev <pashtetdev@gmail.com>
2023-03-05 10:20:27 +0300

    Commit

--- OPD_roguelike/Assets/Scripts/LevelGenerator.cs
+++ OPD_roguelike/Assets/Scripts/LevelGenerator.cs
@@ -1,107 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    /// <summary>
-    /// 0 - пустая
-    /// 1 - стартовая комната
-    /// 2 - лайт с мобами
-    /// 3 - сокровищнеца
-    /// 4 - с боссом
-    /// </summary>
-    private int lines = 0, columns = 0;
-    private int countOfRooms, currentCountOfRooms;
-    private int[,] roomPosition;
     [SerializeField]
-    private int maxCountBoss, maxCountChest;
+    private List<GameObject> rooms;
+    [SerializeField]
+    private List<GameObject> doors;
+    [SerializeField]
+    private GameObject roomPrefab;
+    [SerializeField]
+    private uint countOfRooms = 1, currentCountOfRooms = 0;
+    [SerializeField]
+    private const float verticalRoomOffset = 12f, horizontalRoomOffset = 19f;
     private void Start()
     {
-        lines = countOfRooms = Random.Range(5, 10);
-        columns = countOfRooms = Random.Range(5, 10);
-        roomPosition = new int[lines, columns];
-        SetRooms();
-
-        DrawMap();
+        GetComponent<Room>().Position = Vector2.zero;
+        rooms.Add(gameObject);
+        for (int i = 0; i < transform.childCount; i++)
+            if (transform.GetChild(i).CompareTag("LeftDoor") || transform.GetChild(i).CompareTag("BottomDoor") || transform.GetChild(i).CompareTag("TopDoor") || transform.GetChild(i).CompareTag("RightDoor"))
+                doors.Add(transform.GetChild(i).gameObject);
+        StartCoroutine(Generate());
     }
-    private void SetRooms()
+    IEnumerator Generate()
     {
-        int flagChestRoom = 0, flagBossRoom = 0;
-
-        //Задали начальную комнату
-        int startRoomPosX = 0;
-        int startRoomPosY = Random.Range(0, lines);
-        roomPosition[startRoomPosY, startRoomPosX] = 1;
-
-        //Сетаем все остальные комнаты
-        Vector2 currentroomPosition = new Vector2(startRoomPosX, startRoomPosY); //Текущая комната
-
-        for (int i = 0; i < lines; i++)
+        while (currentCountOfRooms != countOfRooms - 1)
         {
-            for (int j = 0; j < columns; j++)
+            Vector2 positionOffset = Vector2.zero;
+            int numberOfCurrentParentDoor = Random.Range(0, doors.Count);
+            GameObject currentParentDoor = doors[numberOfCurrentParentDoor];
+            switch (currentParentDoor.tag)
             {
-                if (countOfRooms == currentCountOfRooms)
+                case "TopDoor":
+                    positionOffset = Vector2.up;
                     break;
-                int direction = Random.Range(0, 3); //0 - вниз, 1 - направо, 2 - вверх
-                int roomID = 0;
-                int temp = 0;
-                while (temp == 0)
-                {
-                    temp = Random.Range(2, 5);
-                    if (temp == 4 && flagBossRoom < maxCountBoss)
-                        flagBossRoom++;
-                    if (temp == 3 && flagChestRoom < maxCountChest)
-                        flagChestRoom++;
-                }
-                roomID = temp;
-
-                //Переполнение чек
-                switch (direction)
-                {
-                    case 0:
-                        if (currentroomPosition.y + 1 > lines)
-                            break;
-                        else
-                        {
-                            if (roomPosition[(int)currentroomPosition.x, (int)currentroomPosition.y + 1] != 0)
-                                break;
-                            else
-                                currentroomPosition.y++;
-                            break;
-                        }
-                    case 1:
-                        if (currentroomPosition.x + 1 > columns)
-                            break;
-                        else
-                        {
-                            if (roomPosition[(int)currentroomPosition.x + 1, (int)currentroomPosition.y] != 0)
-                                break;
-                            else
-                                currentroomPosition.x++;
-                            break;
-                        }
-                    case 2:
-                        if (currentroomPosition.y - 1 < 0)
-                            break;
-                        else
-                        {
-                            if (roomPosition[(int)currentroomPosition.x, (int)currentroomPosition.y - 1] != 0)
-                                break;
-                            else
-                                currentroomPosition.y--;
-                            break;
-                        }
-                }
-
-                roomPosition[(int)currentroomPosition.x, (int)currentroomPosition.y] = roomID;
-                countOfRooms++;
+                case "BottomDoor":
+                    positionOffset = Vector2.down;
+                    break;
+                case "LeftDoor":
+                    positionOffset = Vector2.left;
+                    break;
+                case "RightDoor":
+                    positionOffset = Vector2.right;
+                    break;
+                default:
+                    break;
             }
-            if (countOfRooms == currentCountOfRooms)
-                break;
+            GameObject currentRoom = Instantiate(roomPrefab, Vector3.zero, Quaternion.Euler(90, 0, 0));
+            currentRoom.GetComponent<Room>().Position = currentParentDoor.transform.parent.GetComponent<Room>().Position + positionOffset;
+            GameObject existingRoom = null;
+            bool intersection = false;
+            for (int i = 0; i < rooms.Count; i++)
+                if (rooms[i].GetComponent<Room>().Position == currentRoom.GetComponent<Room>().Position)
+                {
+                    intersection = true;
+                    existingRoom = rooms[i];
+                    break;
+                }
+            if (intersection)
+            {
+                Debug.Log("Intersection!");
+                Destroy(currentRoom);
+                doors.RemoveAt(numberOfCurrentParentDoor);
+                switch (currentParentDoor.tag)
+                {
+                    case "TopDoor":
+                        currentParentDoor.transform.parent.GetComponent<Room>().topNeighbor = existingRoom;
+                        existingRoom.GetComponent<Room>().bottomNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "BottomDoor":
+                        currentParentDoor.transform.parent.GetComponent<Room>().bottomNeighbor = existingRoom;
+                        existingRoom.GetComponent<Room>().topNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "LeftDoor":
+                        currentParentDoor.transform.parent.GetComponent<Room>().leftNeighbor = existingRoom;
+                        existingRoom.GetComponent<Room>().rightNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "RightDoor":
+                        currentParentDoor.transform.parent.GetComponent<Room>().rightNeighbor = existingRoom;
+                        existingRoom.GetComponent<Room>().leftNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                }
+            }
+            else
+            {
+                currentRoom.name = currentRoom.name + System.Convert.ToString(currentCountOfRooms);
+                currentCountOfRooms++;
+                doors.RemoveAt(numberOfCurrentParentDoor);
+                switch (currentParentDoor.tag)
+                {
+                    case "TopDoor":
+                        ChangingDoors(currentRoom, "BottomDoor");
+                        currentParentDoor.transform.GetComponentInParent<Room>().topNeighbor = currentRoom;
+                        currentRoom.GetComponent<Room>().bottomNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "BottomDoor":
+                        ChangingDoors(currentRoom, "TopDoor");
+                        currentParentDoor.transform.GetComponentInParent<Room>().bottomNeighbor = currentRoom;
+                        currentRoom.GetComponent<Room>().topNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "LeftDoor":
+                        ChangingDoors(currentRoom, "RightDoor");
+                        currentParentDoor.transform.GetComponentInParent<Room>().leftNeighbor = currentRoom;
+                        currentRoom.GetComponent<Room>().rightNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                    case "RightDoor":
+                        ChangingDoors(currentRoom, "LeftDoor");
+                        currentParentDoor.transform.GetComponentInParent<Room>().rightNeighbor = currentRoom;
+                        currentRoom.GetComponent<Room>().leftNeighbor = currentParentDoor.transform.parent.gameObject;
+                        break;
+                }
+                currentRoom.transform.SetParent(currentParentDoor.transform);
+                rooms.Add(currentRoom);
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+        DestroyEmptyDoors();
+        PlacementRoomPosition();
+    }
+
+    private void ChangingDoors(GameObject currentRoom, string tag)
+    {
+        for (int i = 0; i < currentRoom.transform.childCount; i++)
+        {
+            if (currentRoom.transform.GetChild(i).CompareTag(tag))
+                Destroy(currentRoom.transform.GetChild(i).gameObject);
+            else
+            {
+                if (!currentRoom.transform.GetChild(i).CompareTag("Untagged"))
+                    doors.Add(currentRoom.transform.GetChild(i).gameObject);
+            }
         }
     }
-
-    private void DrawMap()
+    private void DestroyEmptyDoors()
     {
+        for(int i = 0; i < doors.Count; i++)
+            if (doors[i].transform.childCount <= 1)
+                Destroy(doors[i]);
+        doors.Clear();
+    }
+    private void PlacementRoomPosition()
+    {
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            rooms[i].transform.position = new Vector3(rooms[i].GetComponent<Room>().Position.x * horizontalRoomOffset,
+                0,
+                rooms[i].GetComponent<Room>().Position.y * verticalRoomOffset);
 
+        }
     }
 }
