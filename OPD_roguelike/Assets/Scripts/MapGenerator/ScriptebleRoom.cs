cac5b5a0d03636c9efa80001257dda23fa5443ed
PashtetDev <pashtetdev@gmail.com>
2023-03-11 13:52:04 +0300

    AddScriptebleRoom

---
+++ OPD_roguelike/Assets/Scripts/MapGenerator/ScriptebleRoom.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptRoom", menuName = "Gameplay/New ScriptRoom")]
+public class ScriptebleRoom : ScriptableObject
+{
+    [SerializeField] private int level;
+
+    [SerializeField] private GameObject spawnPointChest;
+    [SerializeField] private GameObject spawnPointBoss;
+    [SerializeField] private GameObject[] spawnPointsOfMobs;
+    [SerializeField] private GameObject[] spawnPointsOfDecorations;
+
+    private List<Transform> spawnPoints;
+
+    [SerializeField]
+    private ScriptType type;
+    private enum ScriptType
+    {
+        Empty,
+        Start,
+        Chest,
+        Mobs,
+        Boss
+    }
+
+    public ScriptType Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+}
